97d3eb04850bf720dde75f964a64372d07a5213c
Teddy Ngo <teddy.n@itcgroup.io>
2026-03-11 00:07:53 +0700

    fix build spotless

--- auth-service/src/main/java/com/example/auth/service/implementation/AuthServiceImpl.java
+++ auth-service/src/main/java/com/example/auth/service/implementation/AuthServiceImpl.java
@@ -4,7 +4,6 @@ import com.example.auth.client.UserClient;
 import com.example.auth.model.auth.CreateUserRequest;
 import com.example.auth.model.auth.CreateUserResponse;
 import com.example.auth.service.AuthService;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
