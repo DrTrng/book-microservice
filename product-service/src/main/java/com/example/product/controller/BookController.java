5d0fa8e581a2ac33673c6268a95a3b02e3ddae27
DrTrng <96435933+DrTrng@users.noreply.github.com>
2026-03-13 10:57:11 +0700

    ini redis (#20)
    Co-authored-by: Teddy Ngo <teddy.n@itcgroup.io>

--- product-service/src/main/java/com/example/product/controller/BookController.java
+++ product-service/src/main/java/com/example/product/controller/BookController.java
@@ -1,18 +1,22 @@
 package com.example.product.controller;
 
-import com.example.product.model.BookResponse;
-import com.example.product.model.CreateBookRequest;
-import com.example.product.service.BookService;
 import java.util.UUID;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.product.model.BookResponse;
+import com.example.product.model.CreateBookRequest;
+import com.example.product.service.BookService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/books")
@@ -34,5 +38,10 @@ public class BookController {
   @GetMapping
   public Page<BookResponse> getBooks(Pageable pageable) {
     return bookService.getBooks(pageable);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteBook(@PathVariable("id") UUID id) {
+    bookService.deleteBook(id);
   }
 }
